95d8732f3f4478a1a7adf664fe6e577c932af60f
jwringenberg <jonathanringenberg@gmail.com>
2016-11-22 11:36:58 -0600

    Naushil Changes

---
+++ Naushil/txtAdventJava.js
@@ -0,0 +1,8 @@
+function gameEntry()
+{
+	location.href="Page 1.html"
+}
+function name_() {
+	var message = document.getElementById("name_text").value;
+		document.getElementById("display_name").innerHTML = "Welcome."+message
+}
