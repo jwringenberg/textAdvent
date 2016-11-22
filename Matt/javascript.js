a6a211c5263ca2cb0b72d71c26ac4243dcf11342
jwringenberg <jonathanringenberg@gmail.com>
2016-11-22 11:20:19 -0600

    Matts Changes

---
+++ Matt/javascript.js
@@ -0,0 +1,89 @@
+function gameEntry()
+{
+    location.href = "description.html";
+}
+function runaway()
+{
+    location.href = "page 1.html";
+}
+function fight()
+{
+    location.href = "page 2.html";
+}
+function village()
+{
+    location.href = "page 1 d1.html";
+}
+function creature()
+{
+    location.href = "page 1 d2.html";
+}
+function retreat()
+{
+    location.href = "page 2 d1.html";
+}
+function keepgoing()
+{
+    location.href = "page 2 d2.html";
+}
+function soon()
+{
+    location.href = "final battle.html";
+}
+function tommorow()
+{
+    location.href = "final battle.html";
+}
+function fail()
+{
+    location.href = "fail.html";
+}
+function victory()
+{
+    location.href = "victory.html";
+}
+function zeend()
+{
+    location.href = "conclution.html";
+}
+function zegoodending()
+{
+    location.href = "conclution.html";
+}
+function dojo()
+{
+    location.href = "side event 1.html";
+}
+function master()
+{
+    location.href = "side event fail.html";
+}
+function tofinalize()
+{
+    location.href = "side event victory.html";
+}
+function world()
+{
+    location.href = "final battle.html";
+}	
+function doctor()
+{
+    location.href = "the medical hut.html";
+}
+function poison()
+{
+    location.href = "poison.html";
+}
+function healingpotion()
+{
+    location.href = "healing potion.html";
+}
+function ready()
+{
+    location.href = "final battle.html";
+}
+function showname()
+	   {
+	     var message = document.getElementById("aname").value;
+		 document.getElementById("name").innerHTML = message;
+	   }
