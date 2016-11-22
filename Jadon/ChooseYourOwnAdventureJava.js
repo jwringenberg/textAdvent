258d660c2a92eecd95c65fdca5abb25eaa096b82
jwringenberg <jonathanringenberg@gmail.com>
2016-11-22 11:17:15 -0600

    Jabons Changes

---
+++ Jadon/ChooseYourOwnAdventureJava.js
@@ -0,0 +1,55 @@
+function gameEntry()
+{location.href = "BeginAdventure.html";}
+function gameLeave()
+{location.href = "YouQuit.html";}
+function Restart()
+{location.href = "Homepage.html";}
+function PostName()
+{ 
+	var message = document.getElementById("Name12").value;
+	document.getElementById("PlayerName").innerHTML = message;
+}
+function LandofBandits()
+{location.href = "ThievesRoad.html";}
+function DarkKnight()
+{location.href = "BanditKing.html";}
+function GoodGuy()
+{location.href = "Savior.html";}
+function DeadlyKnight()
+{location.href = "DeadOfKnight.html";}
+function KingKnight()
+{location.href = "KingOfTheKnights.html";}
+function LeadBandits()
+{location.href = "BanditRuler.html";}
+function DoNotLeadBandits()
+{location.href = "DeadCorpse.html";}
+function ForestofWolves()
+{location.href = "WolvesLanding.html";}
+function WolfLord()
+{location.href = "LordOfWolves.html";}
+function WolvesMaster()
+{location.href = "WolfChampion.html";}
+function DieByWolf()
+{location.href = "FoodForPups.html";}
+function HeadUpMountain()
+{location.href = "Mountain.html";}
+function Cave()
+{location.href = "DragonSlayer.html";}
+function KillDragon()
+{location.href = "DragonBorn.html";}
+function SlayTheBeast()
+{location.href = "DragonsBlood.html";}
+function BeSlainByTheBeast()
+{location.href = "DragonsFood.html";}
+function DieFromDragon()
+{location.href = "DragonMeat.html";}
+function WolfSage()
+{location.href = "SageOfWolves.html";}
+function SilverStreak()
+{location.href = "RoadKill.html";}
+function Continue()
+{location.href = "Top.html";}
+function Portal()
+{location.href = "Present.html";}
+function DNA()
+{location.href = "Codepins.html"}
