5a91ecdbd47b52ab28af7b54d4607c984e3b79ed
jwringenberg <jonathanringenberg@gmail.com>
2017-10-31 12:08:10 -0500

    Add files via upload

---
+++ Neerav/CYOA.js
@@ -0,0 +1,14 @@
+function gameEntry()
+{location.href = "file:///C:/Users/nchengareddy400/Desktop/CYOA.html";}
+function gameLeave()
+{location.href = "file:///C:/Users/nchengareddy400/Desktop/CYOA.html";}
+function Restart()
+{location.href = "file:///C:/Users/nchengareddy400/Desktop/CYOA.html";}
+	
+	
+	
+	function PostName()
+	{
+		 var message = document.getElementById("Name12").value;
+		 document.getElementById("PlayerName?").innerHTML = message;
+	}
